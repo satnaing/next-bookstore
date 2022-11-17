f846f58366d9b567188fba0f73c4c3e5515eb2eb
satnaing <satnaingdev@gmail.com>
2022-11-18 00:32:05 +0630

    feat: add custom component

---
+++ app/components/CollapsibleMenu.tsx
@@ -0,0 +1,50 @@
+import Link from "next/link"
+import * as Collapsible from "@radix-ui/react-collapsible"
+import * as NavigationMenu from "@radix-ui/react-navigation-menu"
+import MinusIcon from "app/icons/MinusIcon"
+import PlusIcon from "app/icons/PlusIcon"
+
+type Props = {
+  title: string
+  mobile?: boolean
+  menuList: {
+    name: string
+    href: string
+  }[]
+}
+
+const CollapsibleMenu = ({ title, mobile = false, menuList }: Props) => {
+  return (
+    <Collapsible.Root className={`${mobile ? "py-0" : "py-3"}`}>
+      <Collapsible.Trigger
+        className={`flex w-full items-center justify-between p-2 text-lg ${
+          mobile ? "font-medium" : "font-bold"
+        } hover:bg-skin-fill`}
+      >
+        {title}
+        <PlusIcon className="plus-icon" />
+        <MinusIcon className="minus-icon" />
+      </Collapsible.Trigger>
+      <Collapsible.Content>
+        <ul className="flex flex-col px-2 font-sans text-[15px]">
+          {menuList.map(menu => (
+            <li key={menu.name}>
+              <NavigationMenu.Link asChild>
+                <Link
+                  href={menu.href}
+                  className={`underline decoration-dotted hover:decoration-solid ${
+                    mobile ? "block p-2 text-base hover:bg-skin-fill" : ""
+                  }`}
+                >
+                  {menu.name}
+                </Link>
+              </NavigationMenu.Link>
+            </li>
+          ))}
+        </ul>
+      </Collapsible.Content>
+    </Collapsible.Root>
+  )
+}
+
+export default CollapsibleMenu
