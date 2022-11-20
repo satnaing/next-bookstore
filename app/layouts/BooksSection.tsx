fe15ff5b4931405fae2cb6bac2cd3a0e8a7ced20
satnaing <satnaingdev@gmail.com>
2022-11-20 23:02:11 +0630

    feat: add books sections

---
+++ app/layouts/BooksSection.tsx
@@ -0,0 +1,48 @@
+import ItemCard from "@/components/ItemCard"
+import CaretDownIcon from "app/icons/CaretDownIcon"
+import Link from "next/link"
+
+type Props = {
+  title: string
+}
+
+const BooksSection = ({ title }: Props) => {
+  return (
+    <section className="mx-auto max-w-6xl px-4 py-6 md:px-8">
+      <div className="flex items-baseline justify-between">
+        <h2 className="text-2xl font-bold capitalize md:text-3xl">{title}</h2>
+        <SeeAll href="/" />
+      </div>
+      <div className="item-wrapper my-4 grid grid-cols-2 gap-x-4 gap-y-6 sm:grid-cols-3 md:grid-cols-4 md:gap-x-6 lg:grid-cols-5">
+        {[1, 2, 3, 4, 5].map(num => (
+          <ItemCard
+            key={num}
+            className="last:hidden sm:last:flex sm:even:hidden md:last:hidden md:even:flex lg:last:flex"
+          />
+        ))}
+      </div>
+      <div className="mt-8 flex items-center justify-center md:hidden">
+        <SeeAll href="/" bottom />
+      </div>
+    </section>
+  )
+}
+
+type SeeAllType = {
+  href: string
+  bottom?: boolean
+}
+
+const SeeAll = ({ href, bottom = false }: SeeAllType) => (
+  <Link
+    href={href}
+    className={`${
+      bottom ? "flex" : "hidden md:flex"
+    } items-center font-sans font-medium`}
+  >
+    See All
+    <CaretDownIcon className="-rotate-90 scale-75" />
+  </Link>
+)
+
+export default BooksSection
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 import NavBar from "./components/NavBar"
 import TopBar from "./components/TopBar"
+import BooksSection from "./layouts/BooksSection"
 import HeroSection from "./layouts/HeroSection"
 
 export default function Home() {
   return (
-    <div className="h-[1400px]">
+    <div className="h-[1800px]">
       <TopBar />
       <NavBar />
       <main>
         <HeroSection />
+        <BooksSection title="Popular Now" />
+        <BooksSection title="New Books" />
       </main>
     </div>
   )
