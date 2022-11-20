0920336c82ed26d9564006e6c969c54f6a417a25
satnaing <satnaingdev@gmail.com>
2022-11-20 22:56:19 +0630

    fix: update stroke option

--- app/icons/SearchIcon.tsx
+++ app/icons/SearchIcon.tsx
@@ -4,7 +4,7 @@ const SearchIcon = ({ className = "" }: { className?: string }) => {
   const stroke = defaultStroke(className)
   return (
     <svg
-      className={`${className} inline-block h-6 w-6 stroke-black ${stroke}`}
+      className={`${className} inline-block h-6 w-6 ${stroke}`}
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
--- app/utils/utilFuncs.ts
+++ app/utils/utilFuncs.ts
@@ -1,2 +1,2 @@
-export const defaultStroke = (className: string) =>
-  new RegExp("stroke-*", "g").test(className) ? "" : "stroke-2"
+export const defaultStroke = (className: string): string =>
+  new RegExp("stroke-*", "g").test(className) ? "" : "stroke-2 stroke-skin-dark"
