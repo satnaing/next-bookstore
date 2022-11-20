1ba6abfe5cf09ccffd53011f56f7bb5512c0f4d0
satnaing <satnaingdev@gmail.com>
2022-11-20 22:57:52 +0630

    fix: update focus color and border-radius

--- app/components/SearchInput.tsx
+++ app/components/SearchInput.tsx
@@ -10,11 +10,11 @@ const SearchInput = ({ className = "" }: Props) => {
   return (
     <label className={`relative block ${className}`}>
       <span className="sr-only">Search</span>
-      <span className="absolute inset-y-0 right-0 flex items-center bg-skin-accent px-2">
+      <span className="absolute inset-y-0 right-0 flex items-center rounded-r bg-skin-accent px-2">
         <SearchIcon className="stroke-white stroke-2" />
       </span>
       <input
-        className="block w-full border border-skin-card bg-skin-fill py-2 pl-3 pr-12 shadow-sm placeholder:italic placeholder:text-slate-400 focus:border-skin-accent focus:outline-none focus:ring-1 focus:ring-skin-accent sm:text-sm"
+        className="block w-full rounded border border-skin-card bg-skin-fill py-2 pl-3 pr-12 shadow-sm placeholder:italic placeholder:text-slate-400 focus:shadow focus:outline-none focus:ring-1 focus:ring-skin-card sm:text-sm"
         placeholder="Search for any books..."
         type="text"
         name="search"
