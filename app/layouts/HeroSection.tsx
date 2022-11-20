760c603335caa7e65768ce71f5f2b6c0520d849f
satnaing <satnaingdev@gmail.com>
2022-11-20 15:54:36 +0630

    feat: update hero section design for desktop devices

--- app/layouts/HeroSection.tsx
+++ app/layouts/HeroSection.tsx
@@ -5,8 +5,8 @@ import bookPic from "../../public/open-book.webp"
 
 const HeroSection = () => {
   return (
-    <div className="flex flex-col gap-y-4 px-4 py-6">
-      <div className="image-wrapper p-4">
+    <div className="mx-auto flex max-w-6xl flex-col gap-y-4 px-4 py-6 lg:flex-row-reverse lg:px-8 lg:pb-24">
+      <div className="image-wrapper p-4 lg:p-0">
         <Image
           src={bookPic}
           alt="Open Book"
@@ -14,17 +14,18 @@ const HeroSection = () => {
           className="drop-shadow-xl"
         />
       </div>
-      <div className="info-wrapper">
-        <h1 className="font-serif text-4xl font-bold">
+      <div className="info-wrapper flex flex-col gap-y-4 lg:justify-end">
+        <h1 className="font-serif text-4xl font-bold lg:text-6xl">
           Best Place to Find <br /> Your Favourite <br /> Books
         </h1>
-        <p className="my-4 font-sans">
+
+        <p className="font-sans">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
           volutpat odio et dapibus dignissim. Praesent maximus tincidunt
           ultricies. Nam sodales dolor arcu, non venenatis odio tempor eu
         </p>
 
-        <SearchInput className="mt-6 mb-12 w-3/4" />
+        <SearchInput className="mb-4 w-3/4" />
 
         <SocialGroup className="!justify-start" />
       </div>
