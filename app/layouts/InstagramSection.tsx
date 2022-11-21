bb8ee4cc0e1d6537265b2f3f4ae4cf9e3acde8b5
satnaing <satnaingdev@gmail.com>
2022-11-21 17:03:25 +0630

    feat: add Instagram section

---
+++ app/layouts/InstagramSection.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image"
+import igOne from "../../public/instagram-photos/ig_1.jpg"
+import igTwo from "../../public/instagram-photos/ig_2.jpg"
+import igThree from "../../public/instagram-photos/ig_3.jpg"
+import igFour from "../../public/instagram-photos/ig_4.jpg"
+import igFive from "../../public/instagram-photos/ig_5.jpg"
+
+const igPhotos = [
+  { id: 1, image: igOne, desc: "Instagram Photo 1" },
+  { id: 2, image: igTwo, desc: "Instagram Photo 2" },
+  { id: 3, image: igThree, desc: "Instagram Photo 3" },
+  { id: 4, image: igFour, desc: "Instagram Photo 4" },
+  { id: 5, image: igFive, desc: "Instagram Photo 5" },
+]
+
+const InstagramSection = () => {
+  return (
+    <section className="mt-8 flex flex-col items-center gap-y-8 bg-skin-fill px-4 py-12 md:py-14 md:px-8 lg:py-16">
+      <h2 className="text-xl font-bold md:text-3xl lg:text-4xl">
+        # Follow Us on Instagram
+      </h2>
+      <div className="image_container grid grid-cols-2 gap-4 md:grid-cols-4 lg:grid-cols-5 lg:gap-8">
+        {igPhotos.map(photo => (
+          <Image
+            key={photo.id}
+            src={photo.image}
+            alt={photo.desc}
+            className="last:hidden lg:last:block"
+          />
+        ))}
+      </div>
+    </section>
+  )
+}
+
+export default InstagramSection
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import NavBar from "./components/NavBar"
 import TopBar from "./components/TopBar"
 import BooksSection from "./layouts/BooksSection"
 import HeroSection from "./layouts/HeroSection"
+import InstagramSection from "./layouts/InstagramSection"
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
         <HeroSection />
         <BooksSection title="Popular Now" />
         <BooksSection title="New Books" />
+        <InstagramSection />
       </main>
     </div>
   )
