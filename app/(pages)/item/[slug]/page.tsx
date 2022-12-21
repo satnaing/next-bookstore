7504f75b84212fb217938470c78dcc7537afa31c
satnaing <satnaingdev@gmail.com>
2022-12-21 23:38:03 +0630

    feat: update item detail page for desktop devices

--- app/(pages)/item/[slug]/page.tsx
+++ app/(pages)/item/[slug]/page.tsx
@@ -4,6 +4,7 @@ import Footer from "@/common-layouts/Footer"
 import Image from "next/image"
 import sampleBook from "@/public/we-were-liars-book.jpeg"
 import HeartIcon from "@/icons/HeartIcon"
+import SocialGroup from "@/common-components/SocialGroup"
 
 let mockBooks: number[] = []
 for (let index = 1; index < 21; index++) {
@@ -21,53 +22,55 @@ export default function Page({ params, searchParams }: Props) {
       <TopBar />
       <NavBar />
       <main className="padding-x max-width py-6 font-sans">
-        <div className="flex flex-col gap-6">
-          <div className="image-wrapper rounded bg-skin-card p-16 sm:p-8">
+        <div className="flex flex-col gap-6 md:flex-row md:gap-10 lg:gap-16">
+          <div className="image-wrapper mx-auto max-w-[20rem] rounded bg-skin-card p-16 md:w-2/5 md:max-w-none md:self-start">
             <Image src={sampleBook} priority alt="We Were Liars Book" />
           </div>
-          <div>
-            <h1 className="text-xl font-bold">SPY X FAMILY 3</h1>
-            <p className="my-2">
+          <div className="md:w-3/5">
+            <h1 className="text-xl font-bold md:text-2xl">SPY X FAMILY 3</h1>
+            <p className="my-2 md:my-4">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
               volutpat odio et dapibus dignissim. Praesent maximus tincidunt
               ultricies. Nam sodales dolor arcu, non venenatis odio tempor eu
             </p>
-            <p className="my-2">
+            <p className="my-2 md:my-4">
               orem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
               volutpat odio et dapibus dignissim. Praesent maximus tincidunt
               ultricies. Nam sodales dolor arcu, non venenatis odio tempor eu
             </p>
 
-            <hr className="my-4" />
+            <hr className="my-4 md:my-6" />
 
-            <div className="grid grid-cols-2 gap-y-2">
+            <div className="grid grid-cols-2 gap-y-2 md:grid-cols-3 md:gap-y-4 lg:grid-cols-4">
               <div>Book ID :</div>
-              <div>BK 1622018</div>
+              <div className="md:col-span-2 lg:col-span-3">BK 1622018</div>
 
               <div>Author :</div>
-              <div>Tatsuya Endo</div>
+              <div className="md:col-span-2 lg:col-span-3">Tatsuya Endo</div>
 
               <div>Categories :</div>
-              <div>Fiction, Novel, Story</div>
+              <div className="md:col-span-2 lg:col-span-3">
+                Fiction, Novel, Story
+              </div>
 
               <div>Availibility : </div>
-              <div>5 in stock</div>
+              <div className="md:col-span-2 lg:col-span-3">5 in stock</div>
             </div>
 
-            <div className="my-4 flex justify-between">
+            <div className="my-4 flex justify-between md:my-6 md:flex-col-reverse md:gap-y-6">
               <div>
                 <button
                   type="button"
                   title="Reduce Quantity"
-                  className="rounded border bg-skin-card px-2 text-2xl leading-none"
+                  className="rounded border bg-skin-card px-2 text-2xl leading-none md:px-3 md:text-3xl"
                 >
                   -
                 </button>
-                <span className="mx-2">2</span>
+                <span className="mx-2 md:mx-4 md:text-2xl">2</span>
                 <button
                   type="button"
                   title="Reduce Quantity"
-                  className="rounded border bg-skin-card px-2 text-2xl leading-none"
+                  className="rounded border bg-skin-card px-2 text-2xl leading-none md:px-3 md:text-3xl"
                 >
                   +
                 </button>
@@ -75,7 +78,7 @@ export default function Page({ params, searchParams }: Props) {
               <span className="text-xl font-semibold">MMK 7386Ks</span>
             </div>
 
-            <div className="my-6 flex flex-col-reverse gap-4">
+            <div className="my-6 flex flex-col-reverse gap-4 md:flex-row md:gap-8">
               <button
                 type="button"
                 className="flex w-full items-center justify-center gap-x-4 rounded bg-skin-accent py-2 text-center text-lg font-medium text-skin-base"
@@ -90,6 +93,8 @@ export default function Page({ params, searchParams }: Props) {
                 Add To Wishlist
               </button>
             </div>
+
+            <SocialGroup className="!justify-start" />
           </div>
         </div>
       </main>
