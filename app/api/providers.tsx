a1caeb3b052f15e7c35e044c09185916f97abefe
satnaing <satnaingdev@gmail.com>
2023-02-16 12:25:33 +0630

    refactor: add react-query-devtool

--- app/api/providers.tsx
+++ app/api/providers.tsx
@@ -3,11 +3,15 @@
 
 import { useState } from "react"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient())
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
