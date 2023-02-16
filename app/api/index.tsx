cdf305fc964721eec8e7a95948378546435bebc3
satnaing <satnaingdev@gmail.com>
2023-02-16 22:35:38 +0630

    feat: add new fetch methods

--- app/api/index.tsx
+++ app/api/index.tsx
@@ -11,3 +11,31 @@ export async function getBook(slug: string) {
 
   return res.json()
 }
+
+export async function getFeaturedCategories() {
+  const res = await fetch(
+    `http://localhost:1337/api/categories?filters[featured][$eq]=true&sort=featured_order`
+  )
+
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch categories")
+  }
+
+  return res.json()
+}
+
+export async function getBooksBySlug(slug: string, limit = 25) {
+  const res = await fetch(
+    `http://localhost:1337/api/books?filters[categories][slug][$eq]]=${slug}&populate=*&pagination[limit]=${limit}`
+  )
+
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch books")
+  }
+
+  return res.json()
+}
