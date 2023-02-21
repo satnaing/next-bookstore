884ffbe9eb9641dc1bdcf5c7ecc0b3ff505b1ff4
satnaing <satnaingdev@gmail.com>
2023-02-21 09:32:08 +0630

    style: add custom animation for toast component

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,27 @@ module.exports = {
         "upper-md":
           "0 -4px 6px -1px rgb(0 0 0 / 0.1), 0 -2px 4px -2px rgb(0 0 0 / 0.1)",
       },
+      keyframes: {
+        hide: {
+          from: { opacity: 1 },
+          to: { opacity: 0 },
+        },
+        slideIn: {
+          from: {
+            transform: "translateX(calc(100% + var(--viewport-padding)))",
+          },
+          to: { transform: "translateX(0))" },
+        },
+        swipeOut: {
+          from: { transform: "translateX(var(--radix-toast-swipe-end-x))" },
+          to: { transform: "translateX(calc(100% + var(--viewport-padding)))" },
+        },
+      },
+      animation: {
+        hide: "hide 100ms ease-in",
+        slideIn: "slideIn 150ms cubic-bezier(0.16, 1, 0.3, 1)",
+        swipeOut: "swipeOut 100ms ease-out",
+      },
     },
   },
   plugins: [require("@tailwindcss/line-clamp")],
