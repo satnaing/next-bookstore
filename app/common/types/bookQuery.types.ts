9b7aeac1172ed1250d2795e9a9d096fea4b69436
satnaing <satnaingdev@gmail.com>
2023-02-22 18:21:24 +0630

    feat: add custom type for book query

---
+++ app/common/types/bookQuery.types.ts
@@ -0,0 +1,121 @@
+export interface Book {
+  data: BookDatum[]
+  meta: Meta
+}
+
+export interface BookDatum {
+  id: number
+  attributes: PurpleAttributes
+}
+
+export interface PurpleAttributes {
+  title: string
+  description: string
+  price: number
+  createdAt: Date
+  updatedAt: Date
+  publishedAt: Date
+  slug: string
+  in_stock: boolean
+  author_id: AuthorID
+  categories: Categories
+  image: Image
+}
+
+export interface AuthorID {
+  data: Data
+}
+
+export interface Data {
+  id: number
+  attributes: DataAttributes
+}
+
+export interface DataAttributes {
+  name: string
+  description: null
+  createdAt: Date
+  updatedAt: Date
+  slug: string
+}
+
+export interface Categories {
+  data: CategoriesDatum[]
+}
+
+export interface CategoriesDatum {
+  id: number
+  attributes: FluffyAttributes
+}
+
+export interface FluffyAttributes {
+  name: string
+  description: null
+  createdAt: Date
+  updatedAt: Date
+  slug: string
+  featured: boolean
+  featured_order: number
+}
+
+export interface Image {
+  data: ImageDatum[]
+}
+
+export interface ImageDatum {
+  id: number
+  attributes: TentacledAttributes
+}
+
+export interface TentacledAttributes {
+  name: string
+  alternativeText: null
+  caption: null
+  width: number
+  height: number
+  formats: Formats
+  hash: string
+  ext: string
+  mime: string
+  size: number
+  url: string
+  previewUrl: null
+  provider: string
+  provider_metadata: ProviderMetadata
+  createdAt: Date
+  updatedAt: Date
+}
+
+export interface Formats {
+  small: Small
+  thumbnail: Small
+}
+
+export interface Small {
+  name: string
+  hash: string
+  ext: string
+  mime: string
+  path: null
+  width: number
+  height: number
+  size: number
+  url: string
+  provider_metadata: ProviderMetadata
+}
+
+export interface ProviderMetadata {
+  public_id: string
+  resource_type: string
+}
+
+export interface Meta {
+  pagination: Pagination
+}
+
+export interface Pagination {
+  page: number
+  pageSize: number
+  pageCount: number
+  total: number
+}
