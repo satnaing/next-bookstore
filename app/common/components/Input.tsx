41a6d72ab2039a163d1ef8eaff01762e938cd2fd
satnaing <satnaingdev@gmail.com>
2023-03-05 22:03:20 +0630

    feat: add Input component for input fields

---
+++ app/common/components/Input.tsx
@@ -0,0 +1,35 @@
+import { HTMLInputTypeAttribute } from "react"
+import { UseFormRegisterReturn } from "react-hook-form"
+
+type Props = {
+  label: string
+  type?: HTMLInputTypeAttribute
+  placeholder?: string
+  errorMsg?: string
+  register: UseFormRegisterReturn<string>
+}
+
+const Input = ({
+  label,
+  type = "text",
+  placeholder,
+  errorMsg,
+  register,
+}: Props) => {
+  return (
+    <div className="mb-4">
+      <label className="font-sans font-medium">
+        {label}
+        <input
+          type={type}
+          placeholder={placeholder}
+          className="my-1 block w-full rounded border-2 border-[#DCD8D2] bg-skin-base py-1 px-2 font-normal outline-skin-accent"
+          {...register}
+        />
+      </label>
+      {errorMsg && <div className="error">{errorMsg}</div>}
+    </div>
+  )
+}
+
+export default Input
