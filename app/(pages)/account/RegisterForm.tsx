94c0bc5703e31b5aa09f0e5785d388319f461423
satnaing <satnaingdev@gmail.com>
2023-03-08 00:52:09 +0630

    refactor: replace inter Alert with AuthAlert component

--- app/(pages)/account/RegisterForm.tsx
+++ app/(pages)/account/RegisterForm.tsx
@@ -1,17 +1,15 @@
 "use client"
 
-import React from "react"
+import { useState } from "react"
 import Error from "next/error"
-import { useRouter } from "next/navigation"
 import axios, { AxiosError } from "axios"
 import { useMutation } from "@tanstack/react-query"
-import * as AlertDialog from "@radix-ui/react-alert-dialog"
 import { SubmitHandler, useForm } from "react-hook-form"
 import Input from "@/common-components/Input"
 import AlertIcon from "@/icons/AlertIcon"
-import SuccessIcon from "@/icons/SuccessIcon"
 import scrollToTop from "@/utils/scrollToTop"
 import { useAuthStore } from "@/lib/store"
+import AuthAlert from "@/common-components/AuthAlert"
 
 type Inputs = {
   fullName: string
@@ -32,8 +30,8 @@ export default function RegisterForm() {
   } = useForm<Inputs>()
   const { setToken } = useAuthStore()
 
-  const [errorMsg, setErrorMsg] = React.useState<string | null>(null)
-  const [open, setOpen] = React.useState(false)
+  const [errorMsg, setErrorMsg] = useState<string | null>(null)
+  const [open, setOpen] = useState(false)
 
   const mutation = useMutation({
     mutationFn: (userData: Inputs) =>
@@ -209,60 +207,15 @@ export default function RegisterForm() {
           >
             {mutation.isLoading ? "Loading" : "Register"}
           </button>
-          {mutation.isSuccess ? <div>Account registered</div> : null}
-          {/* {mutation.isError ? (
-          <div>
-            An error occurred:{" "}
-            {mutation.error instanceof Error && mutation.error.response}
-          </div>
-        ) : null} */}
         </form>
       </section>
 
-      <AlertModal open={open} setOpen={setOpen} />
+      <AuthAlert
+        open={open}
+        setOpen={setOpen}
+        title="Registration Success"
+        desc="Congratulations! Your account has been successfully created."
+      />
     </>
-  )
-}
-
-type AlertProps = {
-  open: boolean
-  setOpen: React.Dispatch<React.SetStateAction<boolean>>
-}
-
-const AlertModal = ({ open, setOpen }: AlertProps) => {
-  const router = useRouter()
-
-  const handleOutsideClick = () => {
-    setOpen(false)
-    router.push("/")
-  }
-
-  return (
-    <AlertDialog.Root open={open} onOpenChange={() => setOpen(prev => !prev)}>
-      <AlertDialog.Portal>
-        <AlertDialog.Overlay
-          onClick={handleOutsideClick}
-          className="fixed inset-0 z-20 bg-skin-dark bg-opacity-70 backdrop-blur-sm"
-        />
-        <AlertDialog.Content className="fixed top-1/3 left-1/2 z-30 flex max-h-[85vh] w-[90vw] max-w-[400px] -translate-x-1/2 -translate-y-1/2 flex-col items-center rounded bg-white p-6 focus:outline-none">
-          <SuccessIcon className="success stroke h-16 w-16" />
-          <AlertDialog.Title className="success m-0 text-xl font-bold tracking-wider">
-            Registration Success
-          </AlertDialog.Title>
-          <AlertDialog.Description className="mt-4 mb-5 w-64 text-center leading-normal">
-            Congratulations! Your account has been successfully created.
-          </AlertDialog.Description>
-          <AlertDialog.Action asChild autoFocus>
-            <button
-              type="button"
-              onClick={() => router.push("/")}
-              className="focus:shadow-red7 inline-flex h-9 items-center justify-center rounded bg-teal-500 px-4 font-medium leading-none text-white outline-2 outline-offset-2 outline-teal-500 hover:bg-opacity-80 active:bg-opacity-90"
-            >
-              Continue
-            </button>
-          </AlertDialog.Action>
-        </AlertDialog.Content>
-      </AlertDialog.Portal>
-    </AlertDialog.Root>
   )
 }
