c2125fc124324b1b0da973f8700d84c26168a60d
satnaing <satnaingdev@gmail.com>
2023-03-08 15:15:44 +0630

    fix: resolve font not loading on build

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,11 +9,13 @@ import "./globals.css"
 const fraunces = Fraunces({
   variable: "--font-fraunces",
   display: "swap",
+  preload: false, // quick fix for font not loading when build
 })
 
 const quicksand = Quicksand({
   variable: "--font-quicksand",
   display: "swap",
+  preload: false, // https://github.com/vercel/next.js/pull/44594
 })
 
 export default function RootLayout({
