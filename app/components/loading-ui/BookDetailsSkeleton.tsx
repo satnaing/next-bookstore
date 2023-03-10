f1fb4bff31e08af5408c6e15e88833851cca51be
satnaing <satnaingdev@gmail.com>
2023-03-10 21:18:15 +0630

    refactor: update loading skeletons by removing 3rd-party package

--- app/components/loading-ui/BookDetailsSkeleton.tsx
+++ app/components/loading-ui/BookDetailsSkeleton.tsx
@@ -1,7 +1,4 @@
-"use client"
-
-import Skeleton from "react-loading-skeleton"
-import "react-loading-skeleton/dist/skeleton.css"
+import { generateUniqueArray } from "@/utils/utilFuncs"
 
 const BookDetailsSkeleton = () => {
   return (
@@ -11,12 +8,73 @@ const BookDetailsSkeleton = () => {
           className={`image-wrapper mx-auto w-full max-w-[20rem] rounded border-2 border-gray-100 p-8 md:w-2/5 md:max-w-none md:self-start md:p-8 lg:p-16`}
         >
           <div className="relative h-72 w-full overflow-hidden transition-transform duration-500 hover:scale-110 md:h-80 xl:h-96">
-            <Skeleton className="h-full w-full" />
+            <div className="h-full w-full animate-pulse bg-slate-200" />
           </div>
         </div>
         <div className="h-screen md:w-3/5">
-          <div className="h-3/4 overflow-hidden">
-            <Skeleton count={60} />
+          <h1 className="h-8 w-44 animate-pulse bg-slate-200" />
+          <div className="book-desc my-2 md:my-4">
+            {generateUniqueArray(15).map(e => (
+              <div
+                key={e}
+                className="my-2 h-4 animate-pulse rounded-sm bg-slate-200"
+              />
+            ))}
+          </div>
+
+          <hr className="my-4 md:my-6" />
+
+          <div className="grid grid-cols-2 gap-y-2 md:grid-cols-3 md:gap-y-4 lg:grid-cols-4">
+            <div>Author :</div>
+            <div className="md:col-span-2 lg:col-span-3">
+              <div className="h-full animate-pulse rounded-sm bg-slate-200" />
+            </div>
+
+            <div>Categories :</div>
+            <div className="md:col-span-2 lg:col-span-3">
+              <div className="h-full animate-pulse rounded-sm bg-slate-200" />
+            </div>
+
+            <div>Availibility : </div>
+            <div className="md:col-span-2 lg:col-span-3">
+              <div className="h-full animate-pulse rounded-sm bg-slate-200" />
+            </div>
+          </div>
+
+          <div className="my-4 flex justify-between md:my-6 md:flex-col-reverse md:gap-y-6">
+            <div>
+              <button
+                type="button"
+                title="Reduce Quantity"
+                className="rounded border bg-skin-card px-2 text-2xl leading-none md:px-3 md:text-3xl"
+              >
+                -
+              </button>
+              <span className="mx-2 inline-block md:mx-4 md:text-2xl">1</span>
+              <button
+                type="button"
+                title="Reduce Quantity"
+                className="rounded border bg-skin-card px-2 text-2xl leading-none md:px-3 md:text-3xl"
+              >
+                +
+              </button>
+            </div>
+            <span className="text-xl font-semibold">MMK</span>
+          </div>
+
+          <div className="my-6 flex flex-col-reverse gap-4 md:flex-row md:gap-8">
+            <button
+              type="button"
+              className="primary-btn-color flex w-full items-center justify-center gap-x-4 rounded py-2 text-center text-lg font-medium"
+            >
+              Add To Cart
+            </button>
+            <button
+              type="button"
+              className="outline-btn-color flex w-full items-center justify-center gap-x-4 rounded border-2 py-2 text-center text-lg font-medium"
+            >
+              Add To Wishlist
+            </button>
           </div>
         </div>
       </div>
--- app/components/loading-ui/CardSkeletons.tsx
+++ app/components/loading-ui/CardSkeletons.tsx
@@ -1,6 +1,5 @@
-import Skeleton from "react-loading-skeleton"
+import HeartIcon from "@/icons/HeartIcon"
 import { generateUniqueArray } from "@/utils/utilFuncs"
-import "react-loading-skeleton/dist/skeleton.css"
 
 const CardSkeletons = ({ num, slug }: { num: number; slug: string }) => {
   // Generate Unique set of numbers array
@@ -15,10 +14,32 @@ const CardSkeletons = ({ num, slug }: { num: number; slug: string }) => {
 last:hidden sm:last:flex sm:even:hidden md:last:hidden md:even:flex lg:last:flex"
         >
           <div className="p-4 sm:p-8 md:p-4 lg:p-8">
-            <Skeleton className="h-44" />
+            <div className="h-44 animate-pulse bg-slate-200" />
           </div>
-          <div className="px-4 pb-4">
-            <Skeleton count={4} />
+          <div className="content px-4 pb-4">
+            <header className="h-10 line-clamp-2">
+              <h3 className="h-4 w-1/2 animate-pulse bg-slate-200 text-sm" />
+            </header>
+            <div className="price mb-3 font-medium">
+              <div className="h-4 w-2/3 animate-pulse bg-slate-200"></div>
+            </div>
+            <div className="buttons flex gap-x-2">
+              <button
+                type="button"
+                className="flex-1 animate-pulse rounded bg-slate-200 px-1 text-sm font-semibold text-transparent"
+              >
+                Add To Cart
+              </button>
+              <button
+                type="button"
+                className="bg-skin-border-skin-fill basis-1/4 animate-pulse rounded border border-skin-fill p-1"
+                title="Add To Wishlist"
+              >
+                <HeartIcon
+                  className={`!stroke-skin-dark stroke-2 opacity-10`}
+                />
+              </button>
+            </div>
           </div>
         </div>
       ))}
