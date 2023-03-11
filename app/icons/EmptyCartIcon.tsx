f34e33a86f81930f5bb813769077ea18917c8716
satnaing <satnaingdev@gmail.com>
2023-03-11 23:23:48 +0630

    fix: update flood-opacity to floodOpacity

--- app/icons/EmptyCartIcon.tsx
+++ app/icons/EmptyCartIcon.tsx
@@ -37,7 +37,7 @@ const EmptyCartIcon = ({ className = "h-14 w-14" }: { className?: string }) => {
           filterUnits="userSpaceOnUse"
           colorInterpolationFilters="sRGB"
         >
-          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
           <feColorMatrix
             in="SourceAlpha"
             type="matrix"
