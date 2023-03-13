a4664540bd9bc53bda31d50640b2086aa330665b
satnaing <satnaingdev@gmail.com>
2023-03-13 10:46:56 +0630

    feat: add loading UI for book details

---
+++ app/(routes)/item/[slug]/loading.tsx
@@ -0,0 +1,9 @@
+import BookDetailsSkeleton from "@/components/loading-ui/BookDetailsSkeleton"
+
+export default function Loading() {
+  return (
+    <main className="main-container font-sans">
+      <BookDetailsSkeleton />
+    </main>
+  )
+}
