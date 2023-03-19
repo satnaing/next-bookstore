8b0ef0c1973551d0f64393130b2f615704cbeaba
satnaing <satnaingdev@gmail.com>
2023-03-19 18:33:21 +0630

    feat(seo): add meta tags and icons for better SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,63 @@ const quicksand = Quicksand({
   preload: false, // https://github.com/vercel/next.js/pull/44594
 })
 
+export const metadata = {
+  title: {
+    default: "Next Bookstore - Your Ultimate Destination for Books",
+    template: "%s | Next Bookstore",
+  },
+  description:
+    "Discover your next favorite book at Next Bookstore! Browse our wide selection of bestsellers, new releases, and rare finds. Feed your reading passion!",
+  openGraph: {
+    title: {
+      default: "Next Bookstore - Your Ultimate Destination for Books",
+      template: "%s | Next Bookstore",
+    },
+    url: process.env.NEXT_PUBLIC_SITE_URL,
+    images: [
+      {
+        url: `${process.env.NEXT_PUBLIC_SITE_URL}/default-og.jpg`,
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: {
+      default: "Next Bookstore - Your Ultimate Destination for Books",
+      template: "%s | Next Bookstore",
+    },
+    description:
+      "Discover your next favorite book at Next Bookstore! Browse our wide selection of bestsellers, new releases, and rare finds. Feed your reading passion!",
+    creator: "@SatNaingDev",
+    images: [`${process.env.NEXT_PUBLIC_SITE_URL}/default-og.jpg`],
+  },
+  robots: {
+    index: true,
+  },
+  icons: {
+    icon: "icons/icon.png",
+    shortcut: "icons/favicon.ico",
+    apple: "icons/apple-touch-icon.png",
+    other: [
+      {
+        rel: "icon",
+        sizes: "16x16",
+        url: "icons/favicon-16x16.png",
+      },
+      {
+        rel: "icon",
+        sizes: "32x32",
+        url: "icons/favicon-32x32.png",
+      },
+    ],
+  },
+  themeColor: "#EDF4F4",
+}
+
 export default function RootLayout({
   children,
 }: {
