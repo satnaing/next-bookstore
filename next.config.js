f01f43d9381e2d07d07758ff828415746b61f3bb
satnaing <satnaingdev@gmail.com>
2023-06-22 10:30:46 +0630

    build: update dependencies and update next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-    fontLoaders: [
-      { loader: "@next/font/google", options: { subsets: ["latin"] } },
-    ],
-  },
   images: {
     domains: ["res.cloudinary.com"],
   },
